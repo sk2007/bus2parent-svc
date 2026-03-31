4c65a1c77d1f82af2dc86a82ce5d367658d10ce3
sk2007 <sampath.kalagarla@gmail.com>
2026-03-31 15:31:14 -0700

    feat: add createBusParent, getBusParentsByBus, removeBusParent to BusParentDao

--- src/main/java/com/connect/bus2parent/dao/BusParentDao.java
+++ src/main/java/com/connect/bus2parent/dao/BusParentDao.java
@@ -16,6 +16,40 @@ public class BusParentDao {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public List<BusParent> getBusParents() {
-        return namedParameterJdbcTemplate.query("SELECT BusNumber, ParentEmail FROM BUS_PARENT", new HashMap<>(), new BusParentRowMapper());
+        return namedParameterJdbcTemplate.query(
+            "SELECT BusNumber, ParentEmail FROM BUS_PARENT",
+            new HashMap<>(),
+            new BusParentRowMapper()
+        );
+    }
+
+    public List<BusParent> getBusParentsByBus(int busNumber) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("BusNumber", busNumber);
+        return namedParameterJdbcTemplate.query(
+            "SELECT BusNumber, ParentEmail FROM BUS_PARENT WHERE BusNumber=:BusNumber",
+            params,
+            new BusParentRowMapper()
+        );
+    }
+
+    public int createBusParent(BusParent busParent) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("BusNumber", busParent.busID());
+        params.put("ParentEmail", busParent.parentEmail());
+        return namedParameterJdbcTemplate.update(
+            "INSERT INTO BUS_PARENT (BusNumber, ParentEmail) VALUES (:BusNumber, :ParentEmail)",
+            params
+        );
+    }
+
+    public int removeBusParent(int busNumber, String parentEmail) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("BusNumber", busNumber);
+        params.put("ParentEmail", parentEmail);
+        return namedParameterJdbcTemplate.update(
+            "DELETE FROM BUS_PARENT WHERE BusNumber=:BusNumber AND ParentEmail=:ParentEmail",
+            params
+        );
     }
 }
